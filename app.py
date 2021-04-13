419ad4ef6d9b249a12544ebb68fa5200e3801ae9
Anirudh Pai <anirudhpai@Nalinakshs-iMac.local>
2021-04-13 09:34:04 -0700

    finalized comments and reformatted for best resolution screenshots

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flaskr import app, forms # These imports are from the __init__.py file in the folder flaskr.
-from flask import render_template # The render_template function is one provided by the flask framework. It reads an HTML file with Jinja2 templating to integrate a Python backend with an HTML/CSS frontend.
+# The render_template function is one provided by the flask framework. It reads an HTML file with Jinja2 templating to integrate a Python backend with an HTML/CSS frontend.
+from flask import render_template 
 from util.classifier import KNeighborsClassifier # Imported from the classifier.py file in the util folder.
 from util.data_preprocessing import preprocess_data # imported from the data_preprocessing.py file in the util folder.
 
@@ -11,14 +12,16 @@ def home():
     if form.validate_on_submit():
         classifier = KNeighborsClassifier(n_neighbors=int(form.n_neighbors.data))
 
-        form.training_data.data.save('flaskr/data/training_data.csv') # The .save() method is a method that comes with the FlaskForm class provided by the Flask framework. It saves an uploaded file on the inputted file path.
+        # The .save() method is a method that comes with the FlaskForm class provided by the Flask framework. It saves an uploaded file on the inputted file path.
+        form.training_data.data.save('flaskr/data/training_data.csv')
         form.prediction_data.data.save('flaskr/data/prediction_data.csv')
 
         X_train, y_train, test = preprocess_data()
         classifier.fit(X_train, y_train)
         prediction = classifier.predict(test)
 
-    return render_template('home.html', form=form, prediction=prediction) # Using the render_template function to display "home.html" and pass in the form and prediction variables with Jinja2 templating.
+    # Using the render_template function to display "home.html" and pass in the form and prediction variables with Jinja2 templating.
+    return render_template('home.html', form=form, prediction=prediction) 
 
 if __name__ == '__main__':
     app.run(debug=True)
--- util/data_preprocessing.py
+++ util/data_preprocessing.py
@@ -1,15 +1,20 @@
-import numpy as np # Importing the Numpy library. This library provides us with the class "ndarray", which is essentially a two-dimensional list that comes with in-built methods.
-import pandas as pd # Importing the Pandas library. This library will provide functions that allow us to read data from a CSV file stream.
+# Importing the Numpy library. This library provides us with the class "ndarray", which is essentially a two-dimensional list that comes with in-built methods.
+import numpy as np 
+# Importing the Pandas library. This library will provide functions that allow us to read data from a CSV file stream.
+import pandas as pd 
 
 def preprocess_data():
-    # pd.read_csv() is a Pandas function that reads data from a csv file (read from the inputted file path) and places it into a Pandas DataFrame, a special type of data structure.
-    training_data = pd.read_csv('flaskr/data/training_data.csv', header=None) # Header=None simply signifies that there is no headings row in the data.
+    # pd.read_csv() is a Pandas function that reads data from a csv file (read from the inputted file path) 
+    # and places it into a Pandas DataFrame, a special type of data structure.
+
+    # Header=None simply signifies that there is no headings row in the data.
+    training_data = pd.read_csv('flaskr/data/training_data.csv', header=None) 
     prediction_data = pd.read_csv('flaskr/data/prediction_data.csv', header=None)
 
     # Iloc helps us index the Pandas DataFrame similarly to how we would a two-dimensional list. 
-    X_train = training_data.iloc[:, :-1].values # .values is another Pandas DataFrame method. This converts the selected columns (selected using iloc) into a Numpy array. 
+    # .values is another Pandas DataFrame attribute. This converts the selected columns (selected using iloc) into a Numpy array. 
+    X_train = training_data.iloc[:, :-1].values 
     y_train = training_data.iloc[:, -1:].values
 
     test = np.array(prediction_data) # np.array() simply converts a Pandas DataFrame to a Numpy array, which works just like a two-dimensional list.
     return X_train, y_train, test
-
